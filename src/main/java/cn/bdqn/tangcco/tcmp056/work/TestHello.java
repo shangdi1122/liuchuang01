349e6ca164e38755b6e21794fc7d19c3bd19e535
shangdi1122 <1399070052@qq.com>
2017-08-04 11:25:53 +0800

    Initial commit

---
+++ src/main/java/cn/bdqn/tangcco/tcmp056/work/TestHello.java
@@ -0,0 +1,10 @@
+package cn.bdqn.tangcco.tcmp056.work;
+
+/**
+ * Created by THINKPAD on 2017/8/4.
+ */
+public class TestHello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ src/test/java/Test.java
@@ -0,0 +1,7 @@
+/**
+ * Created by THINKPAD on 2017/8/4.
+ */
+public class Test {
+
+
+}
